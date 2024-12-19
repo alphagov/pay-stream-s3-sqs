8d2bb6bfeacdbf363c264f7f0ec2aa92a4dee036
Jonathan Harden <jonathan.harden@digital.cabinet-office.gov.uk>
2024-12-19 09:29:03 +0000

    PP-11677: Remove semicolons from eslint config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,17 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import typescriptEslint from "@typescript-eslint/eslint-plugin"
+import tsParser from "@typescript-eslint/parser"
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+import js from "@eslint/js"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
     baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
     allConfig: js.configs.all
-});
+})
 
 export default [{
     ignores: ["**/*.test.ts"],
@@ -32,4 +32,4 @@ export default [{
         "array-bracket-spacing": [2, "always"],
         "@typescript-eslint/no-empty-function": 0,
     },
-}];
+}]
